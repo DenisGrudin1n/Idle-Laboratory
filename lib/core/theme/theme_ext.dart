bc41b393b6b1b854cacc3b5426ec3ff6f35b164b
Denis <denisgrudininworks@gmail.com>
2026-02-18 00:53:32 +0200

    fix: proper renaming

--- lib/core/theme/theme_ext.dart
+++ lib/core/theme/theme_ext.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:idle_laboratory/core/theme/app_color.dart';
 
-extension ThemeExtension on BuildContext {
+extension ThemeExt on BuildContext {
   AppColor get color => Theme.of(this).extension<AppColor>()!;
 }
